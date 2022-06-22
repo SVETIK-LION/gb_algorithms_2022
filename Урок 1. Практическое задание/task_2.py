4ebf7629606ac9c66c83f2a46bf345169da8106a
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-06-22 22:10:57 +0500

    task_2.py is complete

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -15,3 +15,31 @@
 -- каждый из двух алгоритмов нужно оформить в виде отдельной ф-ции
 -- проставьте сложности каждого выражения в двух ваших алгоритмах
 """
+
+
+lst_1 = [5, -4, 25, 17, 36]
+
+def min_list_element_1(lst: list):
+    for k in range(len(lst) - 1):                    # O(n)
+        for i in range(len(lst) - 1):                # O(n)
+            if lst[i] > lst[i + 1]:                  # O(1)
+                lst[i], lst[i+1] = lst[i+1], lst[i]  # O(1)
+
+    return lst[0]
+
+
+print(min_list_element_1(lst_1))
+
+
+lst_2 = [4, 3, -2, 15, 1]
+
+def min_list_element_2(lst: list):
+    min_element = lst[0]            # O(1)
+    for i in range(len(lst) - 1):   # O(n)
+        if lst[i] < min_element:    # O(1)
+            min_element = lst[i]    # O(1)
+
+    return min_element
+
+
+print(min_list_element_2(lst_2))
