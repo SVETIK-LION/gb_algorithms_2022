c03c626b4b6e655d25c4c9defbdd30a64473730a
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-07-06 22:32:17 +0500

    task_3.py is done

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -26,7 +26,7 @@ companies = {
     'Vault-Tec Corporation': 43893849392923
 }
 
-# Решение 1:
+# Решение №1:
 def three_largest_profit_1(dict):
     sorted_values = sorted(dict.values(), reverse=True) # O(n * log(n)) Сортировка значений словаря
     sorted_dict = {}                                    # O(1)
@@ -42,7 +42,7 @@ def three_largest_profit_1(dict):
 print(three_largest_profit_1(companies))
 
 
-# Решение 2:
+# Решение №2:
 def three_largest_profit_2(dict):
     sorted_keys = sorted(dict, key=dict.get, reverse=True) # O(n * log(n)) Сортировка ключей по их значениям в порядке убывания
     sorted_dict = {}                        # O(1)
@@ -55,24 +55,9 @@ def three_largest_profit_2(dict):
 
 print(three_largest_profit_2(companies))
 
-
-# Решение 3:
-def three_largest_profit_3(dict):
-    largest_profit = {}                                                       # O(1)
-    count = 0                                                                 # O(1)
-
-    for k, v in sorted(dict.items(), key=lambda item: item[1], reverse=True): # O(n) + O(n * log(n))
-        if count < 3:                                                         # O(len(count))
-            largest_profit.setdefault(k, v)                                   # O(1)
-        count += 1                                                            # O(1)
-
-    return largest_profit                                                     # O(1)
-    # Общая сложность: O(n * log(n))
-
-print(three_largest_profit_3(companies))
-
 """
-Решения №2 и 3 самые эффективные из представленных выше.
-
+Решения №2 эффективнее, чем решение №1.
+Так как скорость алгоритма со сложностью O(n * log(n)) меняется менее значительно при увеличении количества значений,
+чем скорость алгоритма со сложностью O(n**2).
 """
 
