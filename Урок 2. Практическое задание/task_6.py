277e5cb5424c8b343c2637b387f5ec6c79ded9a3
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-07-29 21:47:10 +0500

    task_6.py is completed

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -7,3 +7,34 @@
 
 Решите через рекурсию. В задании нельзя применять циклы.
 """
+
+
+from random import randint
+
+
+def guess_number(number, attempts_count):
+    """
+    Игра "Угадай число"
+    :param number: Загаданное число
+    :param attempts_count: Число попыток
+    :return: Результат угадывания
+    """
+    print(f'Попытка № {attempts_count}')
+    user_number = int(input('Введите целое число от 0 до 100: '))
+    if user_number == number:
+        print(f'Вы угадали! Это число {number}')
+    elif attempts_count == 10:
+        print(f'Попытки закончились. Это было число {number}')
+    else:
+        if user_number < number:
+            print(f'Число {user_number} меньше, чем загаданное')
+            return guess_number(number, attempts_count + 1)
+        elif user_number > number:
+            print(f'Число {user_number} больше, чем загаданное')
+            return guess_number(number, attempts_count + 1)
+
+
+try:
+    guess_number(randint(0, 100), 1)
+except ValueError:
+    print('Неверное значение. Введите целое число от 0 до 100')
