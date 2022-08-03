75e609b7e958770f814ef3704520f450459bd39b
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-08-03 10:43:36 +0500

    task_4.py is completed

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -14,3 +14,25 @@
 и одного из алгоритмов, например, sha512
 Можете усложнить задачу, реализовав ее через ООП
 """
+
+
+import hashlib
+from uuid import uuid4
+
+
+cache_url = {}
+salt = uuid4().hex
+
+
+def check_cache(url):
+    if cache_url.get(url):
+        print(f'Адрес {url} уже записан в кэш с хэшем: {cache_url[url]}')
+    else:
+        hash_url = hashlib.sha256(salt.encode() + url.encode()).hexdigest()
+        cache_url[url] = hash_url
+        print(cache_url)
+
+
+check_cache('https://fallout.fandom.com/wiki/Fallout_Wiki')
+check_cache('https://fallout.fandom.com/wiki/Fallout_Wiki')
+check_cache('https://ru.wikipedia.org/wiki/%D0%98%D0%BD%D1%8C_%D0%B8_%D1%8F%D0%BD')
