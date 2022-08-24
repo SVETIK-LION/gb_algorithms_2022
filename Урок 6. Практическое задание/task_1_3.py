0667a138694d3911bbf3bd2bedae75c3ecf44ded
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-08-24 16:13:33 +0300

    task_1_3.py is changed

--- Урок 6. Практическое задание/task_1_3.py
+++ Урок 6. Практическое задание/task_1_3.py
@@ -30,3 +30,61 @@
 
 Это файл для третьего скрипта
 """
+
+
+from pympler import asizeof
+
+
+# Урок 5. Задание 2.1
+# Исходная функция
+class HexCalc_1:
+    def __init__(self, num_1, num_2):
+        self.num_1 = num_1
+        self.num_2 = num_2
+
+    def __add__(self, other):
+        return list(hex(int(''.join(self.num_1), 16) + int(''.join(other.num_2), 16)).upper())[2:]
+
+    def __mul__(self, other):
+        return list(hex(int(''.join(self.num_1), 16) * int(''.join(other.num_2), 16)).upper())[2:]
+
+
+# number_1 = input('Введите первое натуральное шестнадцатеричное число: ')
+# number_2 = input('Введите второе натуральное шестнадцатеричное число: ')
+#
+# sum_nums_1_1 = HexCalc_1(number_1, number_2) + HexCalc_1(number_1, number_2)
+# mul_nums_1_1 = HexCalc_1(number_1, number_2) * HexCalc_1(number_1, number_2)
+#
+# print(f'Сумма чисел: {sum_nums_1_1}\nПроизведение: {mul_nums_1_1}')
+
+obj_1 = HexCalc_1('A', 'B')
+print(asizeof.asizeof(obj_1))
+
+
+# Оптимизированная функция
+class HexCalc_2:
+    __slots__ = ['num_1', 'num_2']
+
+    def __init__(self, num_1, num_2):
+        self.num_1 = num_1
+        self.num_2 = num_2
+
+    def __add__(self, other):
+        return list(hex(int(''.join(self.num_1), 16) + int(''.join(other.num_2), 16)).upper())[2:]
+
+    def __mul__(self, other):
+        return list(hex(int(''.join(self.num_1), 16) * int(''.join(other.num_2), 16)).upper())[2:]
+
+
+# number_1_2 = input('Введите первое натуральное шестнадцатеричное число: ')
+# number_2_2 = input('Введите второе натуральное шестнадцатеричное число: ')
+#
+# sum_nums_2 = HexCalc_2(number_1, number_2) + HexCalc_2(number_1, number_2)
+# mul_nums_2 = HexCalc_2(number_1, number_2) * HexCalc_2(number_1, number_2)
+#
+# print(f'Сумма чисел: {sum_nums_2}\nПроизведение: {mul_nums_2}')
+
+obj_2 = HexCalc_1('A', 'B')
+print(asizeof.asizeof(obj_2))
+
+# Вывод: почему-тоо ничего не поменялось
