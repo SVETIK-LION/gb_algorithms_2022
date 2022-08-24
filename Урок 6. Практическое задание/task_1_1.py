22998f0f2a3705587735ef943b78104ecaf9faf5
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-08-24 14:21:04 +0300

    task_1_1.py is changed

--- Урок 6. Практическое задание/task_1_1.py
+++ Урок 6. Практическое задание/task_1_1.py
@@ -36,15 +36,23 @@ from memory_profiler import profile
 
 
 # Исходная функция
+@profile
 def convert_name_extract_1(list_in: list) -> list:
     """Извлекает имена из элементов и формирует список приветствий."""
     for i in range(len(list_in)):
         list_in[i] = list_in[i].title().split()
         list_in[i] = f"Привет, {list_in[i][-1]}!"
-    list_in =
+    list_out = list_in
+    return list_out
+
+
+my_list_1 = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+result_1 = convert_name_extract_1(my_list_1)
+print(result_1)
 
 
 # Оптимизированная функция
+@profile
 def convert_name_extract_2(list_in: list) -> list:
     """Извлекает имена из элементов и формирует список приветствий."""
     for i in range(len(list_in)):
@@ -53,6 +61,6 @@ def convert_name_extract_2(list_in: list) -> list:
         yield list_in[i]
 
 
-my_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
-result = convert_name_extract(my_list)
-print(list(result))
+my_list_2 = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+result_2 = convert_name_extract_2(my_list_2)
+print(list(result_2))
