420d0d248be514c07e2501f67fd5551d2b7aab33
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-08-24 17:28:39 +0300

    task_1_3.py is completed

--- Урок 6. Практическое задание/task_1_3.py
+++ Урок 6. Практическое задание/task_1_3.py
@@ -37,7 +37,7 @@ from pympler import asizeof
 
 # Урок 5. Задание 2.1
 # Исходная функция
-class HexCalc_1:
+class HexCalcOne:
     def __init__(self, num_1, num_2):
         self.num_1 = num_1
         self.num_2 = num_2
@@ -49,20 +49,20 @@ class HexCalc_1:
         return list(hex(int(''.join(self.num_1), 16) * int(''.join(other.num_2), 16)).upper())[2:]
 
 
-# number_1 = input('Введите первое натуральное шестнадцатеричное число: ')
-# number_2 = input('Введите второе натуральное шестнадцатеричное число: ')
-#
-# sum_nums_1_1 = HexCalc_1(number_1, number_2) + HexCalc_1(number_1, number_2)
-# mul_nums_1_1 = HexCalc_1(number_1, number_2) * HexCalc_1(number_1, number_2)
-#
-# print(f'Сумма чисел: {sum_nums_1_1}\nПроизведение: {mul_nums_1_1}')
+number_1 = input('Введите первое натуральное шестнадцатеричное число: ')
+number_2 = input('Введите второе натуральное шестнадцатеричное число: ')
 
-obj_1 = HexCalc_1('A', 'B')
-print(asizeof.asizeof(obj_1))
+sum_nums_1_1 = HexCalcOne(number_1, number_2) + HexCalcOne(number_1, number_2)
+mul_nums_1_1 = HexCalcOne(number_1, number_2) * HexCalcOne(number_1, number_2)
+
+print(f'Сумма чисел: {sum_nums_1_1}\nПроизведение: {mul_nums_1_1}')
+
+obj_1 = HexCalcOne('A', 'B')
+print(f'Размер объекта №1: {asizeof.asizeof(obj_1)}')
 
 
 # Оптимизированная функция
-class HexCalc_2:
+class HexCalcTwo:
     __slots__ = ['num_1', 'num_2']
 
     def __init__(self, num_1, num_2):
@@ -76,15 +76,18 @@ class HexCalc_2:
         return list(hex(int(''.join(self.num_1), 16) * int(''.join(other.num_2), 16)).upper())[2:]
 
 
-# number_1_2 = input('Введите первое натуральное шестнадцатеричное число: ')
-# number_2_2 = input('Введите второе натуральное шестнадцатеричное число: ')
-#
-# sum_nums_2 = HexCalc_2(number_1, number_2) + HexCalc_2(number_1, number_2)
-# mul_nums_2 = HexCalc_2(number_1, number_2) * HexCalc_2(number_1, number_2)
-#
-# print(f'Сумма чисел: {sum_nums_2}\nПроизведение: {mul_nums_2}')
+number_1_2 = input('Введите первое натуральное шестнадцатеричное число: ')
+number_2_2 = input('Введите второе натуральное шестнадцатеричное число: ')
 
-obj_2 = HexCalc_1('A', 'B')
-print(asizeof.asizeof(obj_2))
+sum_nums_2 = HexCalcTwo(number_1, number_2) + HexCalcTwo(number_1, number_2)
+mul_nums_2 = HexCalcTwo(number_1, number_2) * HexCalcTwo(number_1, number_2)
 
-# Вывод: почему-тоо ничего не поменялось
+print(f'Сумма чисел: {sum_nums_2}\nПроизведение: {mul_nums_2}')
+
+obj_2 = HexCalcTwo('A', 'B')
+print(f'Размер объекта №2: {asizeof.asizeof(obj_2)}')
+
+
+# Размер объекта №1: 376
+# Размер объекта №2: 160
+# Использование слотов уменьшает объем памяти, который заниает объект
