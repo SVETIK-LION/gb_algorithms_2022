24b469ac87ea3a1fb5548fcdae93770d4fce5b17
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-08-24 16:34:49 +0300

    task_1_4.py is completed

--- Урок 6. Практическое задание/task_1_4.py
+++ Урок 6. Практическое задание/task_1_4.py
@@ -32,24 +32,37 @@
 """
 
 
+from timeit import timeit
 from pympler import asizeof
 from numpy import array
+
+
+numbers = [elem for elem in range(1000)]
 
 
 # Урок 4 задание 1
 def func_1(nums):
     new_arr_1 = [i for i in range(len(nums)) if nums[i] % 2 == 0]
-    return new_arr_1
+    return asizeof.asizeof(new_arr_1)
 
 
-print(f'Результат работы оптимизированной функции func_1: {func_1(numbers)}')
-print(f'Время выполнения: {timeit("func_1(numbers)", globals=globals(), number=10000)}')
+print(f'Занимает объект 1: {func_1(numbers)}')
+print(f'Время выполнения func_1: {timeit("func_1(numbers)", globals=globals(), number=10000)}')
 
 
 def func_2(nums):
     new_arr_2 = array([i for i in range(len(nums)) if nums[i] % 2 == 0])
-    return new_arr_2
+    return asizeof.asizeof(new_arr_2)
 
 
-print(f'Результат работы оптимизированной функции func_2: {func_2(numbers)}')
-print(f'Время выполнения: {timeit("func_2(numbers)", globals=globals(), number=10000)}')
+print(f'Занимает объект 2: {func_2(numbers)}')
+print(f'Время выполнения func_2: {timeit("func_2(numbers)", globals=globals(), number=10000)}')
+
+
+# Занимает объект 1: 20208
+# Время выполнения func_1: 6.575020874995971
+# Занимает объект 2: 4128
+# Время выполнения func_2: 0.9490602919977391
+
+# Вывод:
+# После использования arrov из модуля numpy, объект стал занимать меньше памяти.
