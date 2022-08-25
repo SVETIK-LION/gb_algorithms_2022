356abf08f23df92f311b696ca1816b43e6859592
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-08-25 18:40:50 +0300

    task_2_2.py is completed

--- Урок 7. Практическое задание/task_2/task_2_2.py
+++ Урок 7. Практическое задание/task_2/task_2_2.py
@@ -14,3 +14,26 @@
 
 сделайте замеры на массивах длиной 10, 100, 1000 элементов
 """
+
+
+from random import randint
+from timeit import timeit
+
+
+m = 1000
+my_list = [randint(0, 100) for i in range(2 * m + 1)]
+
+
+def not_sort(lst: list):
+    for i in range(len(lst) // 2):
+        lst.remove(max(lst))
+    return max(lst)
+
+
+print(f'Медиана: {not_sort(my_list)}')
+print(f'Время выполнения: {timeit("not_sort(my_list[:])", globals=globals(), number=100)}')
+
+
+# Время выполнения 10: 0.0001703750021988526
+# Время выполнения 100: 0.007357624999713153
+# Время выполнения 1000: 0.38284125000063796
