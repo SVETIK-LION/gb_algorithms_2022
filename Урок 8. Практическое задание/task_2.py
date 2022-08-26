5365e73fd027502f3d3eac683c5783de5a07bfe6
SVETIK-LION <svetlana.lvovskaya@gmail.com>
2022-08-26 11:18:35 +0300

    task_2.py is completed

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -15,20 +15,14 @@
 
 # Делаем исключение для левого потомка
 class LeftKnotExeption(Exception):
-    def __init__(self, left_child):
-        self.left_child = left_child
-
     def __str__(self):
-        return f'Значение {self.left_child} больше, чем должен быть левый потомок'
+        return f'Значение больше, чем должен быть левый потомок'
 
 
 # Делаем исключение для правого потомка
 class RightKnotExeption(Exception):
-    def __init__(self, right_child):
-        self.right_child = right_child
-
     def __str__(self):
-        return f'Значение {self.right_child} больше, чем должен быть левый потомок'
+        return f'Значение меньше, чем должен быть правый потомок'
 
 
 class BinaryTree:
@@ -41,10 +35,10 @@ class BinaryTree:
         self.right_child = None
 
     # добавить левого потомка
-    try:
-        def insert_left(self, new_node):
+    def insert_left(self, new_node):
+        if new_node < self.root:
             # если у узла нет левого потомка
-            if self.left_child == None:
+            if self.left_child is None:
                 # тогда узел просто вставляется в дерево
                 # формируется новое поддерево
                 self.left_child = BinaryTree(new_node)
@@ -55,14 +49,14 @@ class BinaryTree:
                 # и спускаем имеющегося потомка на один уровень ниже
                 tree_obj.left_child = self.left_child
                 self.left_child = tree_obj
-    except LeftKnotExeption as err_l:
-        print(err_l)
+        else:
+            raise LeftKnotExeption
 
     # добавить правого потомка
-    try:
-        def insert_right(self, new_node):
+    def insert_right(self, new_node):
+        if new_node >= self.root:
             # если у узла нет правого потомка
-            if self.right_child == None:
+            if self.right_child is None:
                 # тогда узел просто вставляется в дерево
                 # формируется новое поддерево
                 self.right_child = BinaryTree(new_node)
@@ -73,10 +67,10 @@ class BinaryTree:
                 # и спускаем имеющегося потомка на один уровень ниже
                 tree_obj.right_child = self.right_child
                 self.right_child = tree_obj
-    except RightKnotExeption as err_r:
-        print(err_r)
+        else:
+            raise RightKnotExeption
 
-    # метод доступа к правому потомку
+# метод доступа к правому потомку
     def get_right_child(self):
         return self.right_child
 
@@ -96,10 +90,10 @@ class BinaryTree:
 r = BinaryTree(8)
 print(r.get_root_val())
 print(r.get_left_child())
-r.insert_left(40)
+r.insert_left(74)
 print(r.get_left_child())
 print(r.get_left_child().get_root_val())
-r.insert_right(12)
+r.insert_right(1)
 print(r.get_right_child())
 print(r.get_right_child().get_root_val())
 r.get_right_child().set_root_val(16)
